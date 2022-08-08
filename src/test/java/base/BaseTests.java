b41633d6cac615169e500cf3262ff2e09fd1e992
Jihan95 <jihanahmed41@gmail.com>
2022-08-08 18:34:13 +0200

    New project to push to GitHub

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,44 @@
+package base;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.SearchPage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class BaseTests {
+    private WebDriver driver;
+
+    protected SearchPage searchPage;
+
+    @BeforeClass
+    public void setUP() throws IOException {
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        File file = new File("E://vodafone task data//TestData.xlsx");
+        FileInputStream fis = new FileInputStream(file);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet2 = workbook.getSheetAt(0);
+        String URL = sheet2.getRow(0).getCell(1).toString();
+
+        driver.get(URL);
+        searchPage = new SearchPage(driver);
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
